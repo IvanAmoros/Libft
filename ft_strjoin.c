d96af9698eaa292072621fdcfe7853d4b50b2dad
iamoros- <ivan13ap@gmail.com>
2022-09-15 03:02:21 +0200

    making makefile

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: iamoros- <iamoros-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 20:37:22 by iamoros-          #+#    #+#             */
-/*   Updated: 2022/05/19 18:56:35 by iamoros-         ###   ########.fr       */
+/*   Updated: 2022/09/15 00:46:32 by iamoros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	i;
-	size_t	s;
-	size_t	len;
+	size_t	size;
+	size_t	len1;
 	char	*str;
 
 	i = 0;
-	s = ft_strlen(s1) + ft_strlen(s2);
-	len = ft_strlen(s1);
-	str = (char *)malloc((s + 1) * sizeof(char));
+	size = ft_strlen(s1) + ft_strlen(s2);
+	len1 = ft_strlen(s1);
+	str = (char *)malloc((size + 1) * sizeof(char));
 	if (!str)
 		return (0);
-	while (i < len)
+	while (i < len1)
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	while (i < s)
+	while (i < size)
 	{
 		str[i] = *s2;
 		i++;
